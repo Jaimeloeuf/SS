23f657336870b5cb266932e5071c12380810bae3
Jaime <jaimeloeuf@gmail.com>
2021-01-19 23:15:05 +0800

    [rust-i] Clean up TokenType, removing display trait impl:     Using debug formatter directly instead.

--- rust/src/token_type.rs
+++ rust/src/token_type.rs
@@ -1,9 +1,11 @@
-//  @todo Used for 'impl' of fmt::Display for TokenType enum
-use std::fmt;
-
-// PartialEq is used to do comparison instead of match. Refer to Parser struct methods
+/**
+ * Enum for all possible Token Types.
+ *
+ * Inherited traits:
+ * Debug is to make debug printing easier and skip implementing the display trait
+ * PartialEq is used to do comparison instead of match. Refer to Parser struct methods
+ */
 #[derive(Debug, Clone, PartialEq)]
-#[allow(dead_code)] // @todo Remove once scanner complete
 pub enum TokenType {
   // Single-character tokens.
   Semicolon,
@@ -65,52 +67,4 @@ pub enum TokenType {
   Null, // to remove?
 
   Eof,
-}
-
-impl fmt::Display for TokenType {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    match self {
-      // @todo Instead of match, should write its literal value instead
-      TokenType::Semicolon => write!(f, "Semicolon"),
-      TokenType::LeftParen => write!(f, "LeftParen"),
-      TokenType::RightParen => write!(f, "RightParen"),
-      TokenType::LeftBrace => write!(f, "LeftBrace"),
-      TokenType::RightBrace => write!(f, "RightBrace"),
-      TokenType::Comma => write!(f, "Comma"),
-      TokenType::Dot => write!(f, "Dot"),
-      TokenType::Minus => write!(f, "Minus"),
-      TokenType::Plus => write!(f, "Plus"),
-      TokenType::Slash => write!(f, "Slash"),
-      TokenType::Star => write!(f, "Star"),
-      TokenType::Bang => write!(f, "Bang"),
-      TokenType::BangEqual => write!(f, "BangEqual"),
-      TokenType::Equal => write!(f, "Equal"),
-      TokenType::EqualEqual => write!(f, "EqualEqual"),
-      TokenType::Greater => write!(f, "Greater"),
-      TokenType::GreaterEqual => write!(f, "GreaterEqual"),
-      TokenType::Less => write!(f, "Less"),
-      TokenType::LessEqual => write!(f, "LessEqual"),
-      TokenType::And => write!(f, "And"),
-      TokenType::Or => write!(f, "Or"),
-      TokenType::Identifier => write!(f, "Identifier"),
-      TokenType::Str => write!(f, "Str"),
-      TokenType::Number => write!(f, "Number"),
-      TokenType::Break => write!(f, "Break"),
-      TokenType::If => write!(f, "If"),
-      TokenType::Else => write!(f, "Else"),
-      TokenType::False => write!(f, "False"),
-      TokenType::Function => write!(f, "Function"),
-      TokenType::For => write!(f, "For"),
-      TokenType::While => write!(f, "While"),
-      TokenType::Print => write!(f, "Print"),
-      TokenType::Return => write!(f, "Return"),
-      TokenType::True => write!(f, "True"),
-      TokenType::Const => write!(f, "Const"),
-      TokenType::Null => write!(f, "Null"),
-      TokenType::Eof => write!(f, "Eof"),
-
-      #[allow(unreachable_patterns)]
-      _ => write!(f, "SS: Either Display trait not implemented yet or invalid"),
-    }
-  }
 }
