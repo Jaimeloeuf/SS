b6274e390a5c6c89b8dbc10445645417e6623bd1
Jaime <jaimeloeuf@gmail.com>
2021-02-15 21:28:50 +0800

    [rust-i] Fix token display trait for Identifier:     Stop Display trait from printing Identifier token types as simple     string token types just because it has a string literal.

--- rust/src/token.rs
+++ rust/src/token.rs
@@ -76,9 +76,11 @@ impl std::fmt::Display for Token {
         if self.literal.is_none() {
             write!(f, "{:?}", self.token_type)
         } else {
-            // Special way to print string literals
             match self.literal.as_ref().unwrap() {
-                Literal::String(ref string) => write!(f, "String '{}'", string),
+                // Special way to print string literals belong to "String" token type and not a Identifier
+                Literal::String(ref string) if self.token_type != TokenType::Identifier => {
+                    write!(f, "String '{}'", string)
+                }
                 none_string => write!(f, "{:?} {}", self.token_type, none_string),
             }
         }
