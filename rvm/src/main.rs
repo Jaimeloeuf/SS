5b99b8d398d0f2c0e41927b24d51d4f07e46d5c5
Jaime <jaimeloeuf@gmail.com>
2021-03-11 23:01:13 +0800

    [rvm] Update main to read file: 1.  Rename old main function testing vm with hand written chunks to be     a test function in main.rs 2.  Create new main function to read program arguments to read a file.

--- rvm/src/main.rs
+++ rvm/src/main.rs
@@ -1,3 +1,6 @@
+use std::env;
+use std::fs;
+
 mod chunk;
 mod debug;
 mod error;
@@ -12,10 +15,26 @@ use value::Value;
 use vm::VM;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1];
+    println!("Entering file '{}'", filename);
+
+    let source = match fs::read_to_string(filename) {
+        Ok(source) => source,
+        Err(err) => panic!("Unable to read file: {}", err), // Bubble error up instead of panic!
+    };
+}
+
+fn test_vm_with_chunk() {
     let mut chunk = Chunk::new();
 
     chunk.write(OpCode::CONSTANT(Value::Number(1.2)), 2);
     chunk.write(OpCode::NEGATE, 2);
+
+    // chunk.write(OpCode::CONSTANT(Value::Null), 3);
+    // chunk.write(OpCode::CONSTANT(Value::Number(4.2)), 3);
+    // chunk.write(OpCode::ADD, 3);
 
     chunk.write(OpCode::CONSTANT(Value::Number(1.8)), 3);
     chunk.write(OpCode::SUBTRACT, 3);
