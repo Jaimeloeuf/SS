f55425b18b54f3601bc2c318e0da7fc6798b7da4
Jaime <jaimeloeuf@gmail.com>
2021-03-17 01:35:32 +0800

    [rvm] Implement static rules table.

--- rvm/src/compiler/parse_rule.rs
+++ rvm/src/compiler/parse_rule.rs
@@ -35,3 +35,34 @@ impl ParseRule {
         }
     }
 }
+
+// The problem with this approach is that,
+// 1. Unlike a static rules table, where we access the ParseRule directly with O(1) speed, this function have a much higher runtime cost
+// 2. Unlike accessing a static rules table, everytime a rule is requested, a new ParseRule object is created
+// fn get_rule(token_type: TokenType) -> ParseRule {
+//     match token_type {
+//         TokenType::RightParen => ParseRule::new(Precedence::None),
+//         _ => panic!("parse rule missing"),
+//     }
+// }
+
+// This is the same thing as the one used, only difference is that,
+// 1. The alternative is more self documenting, as it explicitly inserts the ParseRule using the token type as index
+// 2. This is more dangerous, as a change in TokenType enum's ordering will cause wrong ParseRules to be matched to the TokenTypes
+// static rule_table: [ParseRule; 1] = [ParseRule::new(Precedence::None)];
+
+// Static so that only 1 instance of this table in memory
+// Create the table internally by inserting rules using TokenType as index 1 by 1
+// This has no runtime cost as this is a static value evaluted at compile time
+pub static rules_table: [ParseRule; 1] = {
+    // Same type as rule table, need to initialize it, so using default empty ParseRule
+    let rules_array: [ParseRule; 1] = [ParseRule::new(Precedence::None); 1];
+
+    /*
+        Insert rules for each token type 1 by 1
+        TokenType enum variants converted to usize first before using it to index the array
+    */
+    rules_array[TokenType::Semicolon as usize] = ParseRule::new(Precedence::None);
+
+    rules_array
+};
