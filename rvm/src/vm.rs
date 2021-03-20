5a475461af348d272ab5c5036eeb2992f32bd0a9
Jaime <jaimeloeuf@gmail.com>
2021-03-20 18:13:27 +0800

    [rvm] Update VM binary op macros: 1.  Update generic binary op macro to take in a string literal as the     name of the actual binary operation used in error logging to debug 2.  Update the binary op macros to pass in name of operation and add     docs for their match arm and arm logic.

--- rvm/src/vm.rs
+++ rvm/src/vm.rs
@@ -26,11 +26,12 @@ pub enum InterpretResult {
 // Macro to perform any generic binary operation on the last 2 values on the stack
 // This macro should only used by the other binary operation macros
 macro_rules! generic_binary_op {
+    // $op_name     -> String literal name for the actual binary operation, used in error output for debugging
     // $stack       -> Takes in the identifier for the stack value too
     // $operator    -> accepts a TokenTree -> Single Token -> Punctuation -> https://doc.rust-lang.org/reference/tokens.html#punctuation
     // $arm_pattern -> A match arm pattern for the types of values expected of the last 2 values on the stack
     // $arm_logic   -> An expression to execute and return if the last 2 values on the stack matched the arm_pattern
-    ($stack:ident, $operator:tt, $arm_pattern:pat => $arm_logic:expr) => {
+    ($op_name:literal, $stack:ident, $operator:tt, $arm_pattern:pat => $arm_logic:expr) => {
         // Pop the operands off the stack in reverse order, since the first operand will be loaded first
         // Loading will be, LOAD A, LOAD B, since stack is LIFO, when we pop out 2 values, it will be B then A
         let b = $stack.pop();
@@ -39,7 +40,7 @@ macro_rules! generic_binary_op {
         // Only run this check during debug builds, assuming correctly generated OpCodes will not have this issue
         #[cfg(debug_assertions)]
         if a.is_none() || b.is_none(){
-            panic!(format!("VM Debug Error: Stack missing values for binary operation '{}'", stringify!($operator)));
+            panic!(format!("VM Debug Error: Stack missing values for {} operation '{}'",  $op_name, stringify!($operator)));
         }
 
         match (a, b) {
@@ -51,8 +52,8 @@ macro_rules! generic_binary_op {
                 // Unwrap the values directly assuming that they are definitely Some() variants
                 // If it fails, it means opcodes are generated wrongly where the stack is missing values needed for the opcode
                 return Err(RuntimeError::TypeError(format!(
-                    "Invalid operand types {:?} and {:?} used for '{}' operation",
-                    a.unwrap(), b.unwrap(), stringify!($operator)
+                    "Invalid operand types {:?} and {:?} used for '{}' {} operation",
+                    a.unwrap(), b.unwrap(), stringify!($operator), $op_name
                 )))
         }
     }
@@ -63,7 +64,14 @@ macro_rules! arithmetic_binary_op {
     // $stack ->  Takes in the identifier for the stack value too
     // $operator -> accepts a TokenTree -> Single Token -> Punctuation -> https://doc.rust-lang.org/reference/tokens.html#punctuation
     ($stack:ident, $operator:tt) => {{
-        generic_binary_op!($stack, $operator, (Some(Value::Number(num1)), Some(Value::Number(num2))) => $stack.push(Value::Number(num1 $operator num2)));
+        generic_binary_op!(
+            "Arithmetic",
+            $stack,
+            $operator,
+
+            // Expect last 2 values on stack to be numbers, pushes a number back onto the stack
+            (Some(Value::Number(num1)), Some(Value::Number(num2))) => $stack.push(Value::Number(num1 $operator num2))
+        );
     }};
 }
 
@@ -72,7 +80,14 @@ macro_rules! equality_op {
     // $stack ->  Takes in the identifier for the stack value too
     // $operator -> accepts a TokenTree -> Single Token -> Punctuation -> https://doc.rust-lang.org/reference/tokens.html#punctuation
     ($stack:ident, $operator:tt) => {{
-        generic_binary_op!($stack, $operator, (Some(value1), Some(value2)) => $stack.push(Value::Bool(value1 $operator value2)));
+        generic_binary_op!(
+            "Equality",
+            $stack,
+            $operator,
+
+            // Last 2 values on stack can be any Value enum variant, compares directly using Value's derived PartialEq trait and pushes a Bool back onto the stack
+            (Some(value1), Some(value2)) => $stack.push(Value::Bool(value1 $operator value2))
+        );
     }};
 }
 
@@ -81,7 +96,14 @@ macro_rules! numeric_comparison_op {
     // $stack ->  Takes in the identifier for the stack value too
     // $operator -> accepts a TokenTree -> Single Token -> Punctuation -> https://doc.rust-lang.org/reference/tokens.html#punctuation
     ($stack:ident, $operator:tt) => {{
-        generic_binary_op!($stack, $operator, (Some(Value::Number(num1)), Some(Value::Number(num2))) => $stack.push(Value::Bool(num1 $operator num2)));
+        generic_binary_op!(
+            "Numeric Comparison",
+            $stack,
+            $operator,
+
+            // Expect last 2 values on stack to be numbers, pushes a bool back onto the stack
+            (Some(Value::Number(num1)), Some(Value::Number(num2))) => $stack.push(Value::Bool(num1 $operator num2))
+        );
     }};
 }
 
