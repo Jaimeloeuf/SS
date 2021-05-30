01f6bf11a32a2223f0a7cf0d127a8ed34b8e4a54
Jaime <jaimeloeuf@gmail.com>
2021-05-30 21:58:09 +0800

    stlc: Refactor type checker:     Refactor type checker to use switch instead of if/else-if statements     for the different AST types

--- Simply Typed Lambda Calculus/src/check.js
+++ Simply Typed Lambda Calculus/src/check.js
@@ -8,13 +8,11 @@ function typeEq(a, b) {
   // If both are arrays, it means that both are functions types
   if (a instanceof Array && b instanceof Array) {
     // Check if the length/depth of the functions/abstractions are the same, if not same, means diff type
-    if (a.length !== b.length)
-      return false;
+    if (a.length !== b.length) return false;
 
     // Check every type of the functions/abstractions individually and recursively to ensure that they are all the same type
     // If any type in the array is different, then not equal types
-    for (let i = 0; i < a.length; i += 1)
-      if (!typeEq(a[i], b[i])) return false;
+    for (let i = 0; i < a.length; i += 1) if (!typeEq(a[i], b[i])) return false;
 
     // If all the same, then the functions/abstractions are of the same type
     return true;
@@ -36,63 +34,69 @@ module.exports.Check = function Check(ast, diagnostics = []) {
 
   console.log(ast);
 
-  // Literals:
-  // - 0 is of type Natural
-  // - false and true are of type Boolean
-  // Everything else is incorrect.
-  if (ast.type === ASTNodes.Literal) {
-    if (ast.value === 0)
-      return {
-        diagnostics,
-        type: Types.Natural,
-      };
-    else if (ast.value === false || ast.value === true)
-      return {
-        diagnostics,
-        type: Types.Boolean,
-      };
-    else {
-      diagnostics.push("Unknown type literal");
-      return { diagnostics };
+  switch (ast.type) {
+    // Literals:
+    // - 0 is of type Natural
+    // - false and true are of type Boolean
+    // Everything else is incorrect.
+    case ASTNodes.Literal: {
+      if (ast.value === 0)
+        return {
+          diagnostics,
+          type: Types.Natural,
+        };
+      else if (ast.value === false || ast.value === true)
+        return {
+          diagnostics,
+          type: Types.Boolean,
+        };
+      else {
+        diagnostics.push("Unknown type literal");
+        return { diagnostics };
+      }
     }
 
-    // We get the type of identifier from the symbol table
-  } else if (ast.type === ASTNodes.Identifier) {
-    return {
-      diagnostics,
-      type: SymbolTable.lookup(ast.name),
-    };
+    // Get the type of identifier from the symbol table
+    case ASTNodes.Identifier:
+      return {
+        diagnostics,
+        type: SymbolTable.lookup(ast.name),
+      };
 
     // if-then-else block is correct if:
     // - The condition is of type Boolean.
-    // - Then and else are of the same type.
-  } else if (ast.type === ASTNodes.Condition) {
-    if (!ast.then || !ast.el || !ast.condition) {
-      diagnostics.push("No condition for a conditional expression");
-      return { diagnostics };
-    }
+    // - The expressions of 'then' and 'else' are of the same type.
+    case ASTNodes.Condition: {
+      if (!ast.then || !ast.el || !ast.condition) {
+        diagnostics.push("No condition for a conditional expression");
+        return { diagnostics };
+      }
 
-    const c = Check(ast.condition);
-    diagnostics = diagnostics.concat(c.diagnostics);
-    const conditionType = c.type;
-    if (!typeEq(conditionType, Types.Boolean)) {
-      diagnostics.push("Incorrect type of condition of condition");
-      return { diagnostics };
-    }
+      // Boolean type check for condition
+      const c = Check(ast.condition);
+      diagnostics = diagnostics.concat(c.diagnostics);
+      const conditionType = c.type;
+      if (!typeEq(conditionType, Types.Boolean)) {
+        diagnostics.push("Incorrect type of condition of condition");
+        return { diagnostics };
+      }
 
-    const thenBranch = Check(ast.then);
-    diagnostics = diagnostics.concat(thenBranch.diagnostics);
-    const thenBranchType = thenBranch.type;
+      // Get type of then expression
+      const thenBranch = Check(ast.then);
+      diagnostics = diagnostics.concat(thenBranch.diagnostics);
+      const thenBranchType = thenBranch.type;
 
-    const elseBranch = Check(ast.el);
-    diagnostics = diagnostics.concat(elseBranch.diagnostics);
-    const elseBranchType = elseBranch.type;
+      // Get type of else expression
+      const elseBranch = Check(ast.el);
+      diagnostics = diagnostics.concat(elseBranch.diagnostics);
+      const elseBranchType = elseBranch.type;
 
-    if (typeEq(thenBranchType, elseBranchType))
-      return thenBranch;
-    else {
-      diagnostics.push("Incorrect type of then/else branches");
-      return { diagnostics };
+      // Check if both branches are of equal type
+      if (typeEq(thenBranchType, elseBranchType)) return thenBranch;
+      else {
+        diagnostics.push("Incorrect type of then/else branches");
+        return { diagnostics };
+      }
     }
 
     // Abstraction registers its argument in the SymbolTable
@@ -100,85 +104,93 @@ module.exports.Check = function Check(ast, diagnostics = []) {
     // - The type of its argument.
     // - Type of its body, which may depend on the type
     // of the argument registered in the SymbolTable.
-  } else if (ast.type === ASTNodes.Abstraction) {
-    const scope = new Scope();
-    scope.add(ast.arg.id.name, ast.arg.type);
-    SymbolTable.push(scope);
-    if (!ast.body) {
-      diagnostics.push("Missing function body");
-      return { diagnostics };
-    }
+    case ASTNodes.Abstraction: {
+      const scope = new Scope();
+      scope.add(ast.arg.id.name, ast.arg.type);
+      SymbolTable.push(scope);
+      if (!ast.body) {
+        diagnostics.push("Missing function body");
+        return { diagnostics };
+      }
 
-    const body = Check(ast.body);
-    const bodyType = body.type;
-    diagnostics = diagnostics.concat(body.diagnostics);
-    if (!bodyType) {
-      diagnostics.push("Incorrect type of the body");
-      return { diagnostics };
+      const body = Check(ast.body);
+      const bodyType = body.type;
+      diagnostics = diagnostics.concat(body.diagnostics);
+      if (!bodyType) {
+        diagnostics.push("Incorrect type of the body");
+        return { diagnostics };
+      }
+      return {
+        diagnostics,
+        type: [ast.arg.type, bodyType],
+      };
     }
-    return {
-      diagnostics,
-      type: [ast.arg.type, bodyType],
-    };
 
     // The type of IsZero is Boolean but in case
     // its argument is not Natural the program is incorrect.
-  } else if (ast.type === ASTNodes.IsZero) {
-    const body = Check(ast.expression);
-    diagnostics = diagnostics.concat(body.diagnostics);
-    const bodyType = body.type;
-    if (!typeEq(bodyType, Types.Natural)) {
-      diagnostics.push("Incorrect type of IsZero");
-      return { diagnostics };
-    }
+    case ASTNodes.IsZero: {
+      const body = Check(ast.expression);
+      diagnostics = diagnostics.concat(body.diagnostics);
+      const bodyType = body.type;
+      if (!typeEq(bodyType, Types.Natural)) {
+        diagnostics.push("Incorrect type of IsZero");
+        return { diagnostics };
+      }
 
-    return {
-      diagnostics,
-      type: Types.Boolean,
-    };
+      return {
+        diagnostics,
+        type: Types.Boolean,
+      };
+    }
 
     // The type of the arithmetic operations are Natural
     // but in case the type of the body is not the entire
     // program is incorrect.
-  } else if (ast.type === ASTNodes.Arithmetic) {
-    const body = Check(ast.expression);
-    diagnostics = diagnostics.concat(body.diagnostics);
-    const bodyType = body.type;
-    if (!typeEq(bodyType, Types.Natural)) {
-      diagnostics.push(`Incorrect type of ${ast.operation}`);
-      return { diagnostics };
+    case ASTNodes.Arithmetic: {
+      const body = Check(ast.expression);
+      diagnostics = diagnostics.concat(body.diagnostics);
+      const bodyType = body.type;
+      if (!typeEq(bodyType, Types.Natural)) {
+        diagnostics.push(`Incorrect type of ${ast.operation}`);
+        return { diagnostics };
+      }
+      return {
+        diagnostics,
+        type: Types.Natural,
+      };
     }
-    return {
-      diagnostics,
-      type: Types.Natural,
-    };
 
     // The type of:
     // e1: T1, e2: T2, e1 e2: T1
-  } else if (ast.type === ASTNodes.Application) {
-    const l = Check(ast.left);
-    const leftType = l.type || [];
-    diagnostics = diagnostics.concat(l.diagnostics);
+    case ASTNodes.Application: {
+      const l = Check(ast.left);
+      const leftType = l.type || [];
+      diagnostics = diagnostics.concat(l.diagnostics);
 
-    const r = Check(ast.right);
-    const rightType = r.type || [];
-    diagnostics = diagnostics.concat(r.diagnostics);
+      const r = Check(ast.right);
+      const rightType = r.type || [];
+      diagnostics = diagnostics.concat(r.diagnostics);
 
-    if (leftType.length)
-      if (!ast.right || leftType[0] === rightType) {
-        return {
-          diagnostics,
-          type: leftType[1],
-        };
-      } else {
-        diagnostics.push("Incorrect type of application");
-        return {
-          diagnostics,
-        };
-      }
-    else
+      if (leftType.length)
+        if (!ast.right || leftType[0] === rightType) {
+          return {
+            diagnostics,
+            type: leftType[1],
+          };
+        } else {
+          diagnostics.push("Incorrect type of application");
+          return {
+            diagnostics,
+          };
+        }
+      else return { diagnostics };
+    }
+
+    default:
+      // console.log("Unknown AST Type: ", ast.type);
+      // process.exit(1);
+
+      console.log("Unknown AST Type: ", ast.type);
       return { diagnostics };
   }
-
-  return { diagnostics };
 };
