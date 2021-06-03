dc61ea635b00e0d12988f54d3c557a19225c6b37
Jaime <jaimeloeuf@gmail.com>
2021-06-03 22:38:28 +0800

    ri: Add Type::Lazy, implement PartialEq and misc: 1.  Add a new Lazy type, to experiment with delaying type checking for     the function parameters during function definitions, and to defer     type checking to when a function call is being checked, making the     type checking process 'lazy' till it is needed for a function call. 2.  Implement PartialEq trait for Type enum to be able to compare types     directly using equality check operator, '==' instead of using a type     checking function like stlc, and to also allow some interesting     checking ways such as how Type::Lazy is equal to all other types. 3.  Update Type::Function to store number of parameters instead of a vec     of parameter types, since there is no way to tell what types are the     parameters without explicit type annotations or some form of advance     inference that is beyond me right now. This will be used together with     Type::Lazy, to delay checking function type till a function call.

--- ri/src/type_checker/structs.rs
+++ ri/src/type_checker/structs.rs
@@ -18,22 +18,49 @@ pub struct TypeChecker {
 /// An enum of all possible types of values in SS
 ///
 /// Need clone trait tmp in TypeChecker's symbol table
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, Clone)]
 pub enum Type {
     Number,
     String,
     Bool,
     Null,
 
+    Lazy,
+
     /// Arrays expect homogenous data types
     Array(Box<Type>),
 
-    /// Func(vector_of_parameter_types, return_type)
-    Func(Vec<Box<Type>>, Box<Type>),
+    /// Func(number_of_parameters, return_type)
+    Func(usize, Box<Type>),
 
     /// Return is a special type that wraps a Type,
     /// The point of the Return type is to allow type checker to know and let it bubble up till a handler
     Return(Box<Type>),
 }
 
-// @todo Manually implement PartialEq for the higher level types like Functions and Arrays..?
+// Manually implement PartialEq trait for higher level types like Functions and Arrays that cannot be directly compared
+impl PartialEq for Type {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            // Every type is Equals to Lazy, since the Lazy type is just a place holder
+            (&Type::Lazy, _) | (_, &Type::Lazy) => true,
+
+            // For primitive types, as long as they are the same, they are equal
+            (&Type::Number, &Type::Number)
+            | (&Type::String, &Type::String)
+            | (&Type::Bool, &Type::Bool)
+            | (&Type::Null, &Type::Null) => true,
+
+            // For arrays, ensure that the types of their elements match
+            (&Type::Array(ref array_element_type_1), &Type::Array(ref array_element_type_2)) => {
+                // Not too sure if these work, since the ele type are Boxed values
+                array_element_type_1 == array_element_type_2
+            }
+
+            // (&Type::Func(ref f), &Type::Func(ref other)) => Rc::ptr_eq(f, other),
+            // (&Type::Class(ref c), &Type::Class(ref other)) => Rc::ptr_eq(c, other),
+            // (&Type::Instance(ref i), &Type::Instance(ref other)) => Rc::ptr_eq(i, other),
+            _ => false,
+        }
+    }
+}
