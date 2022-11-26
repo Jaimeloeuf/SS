85feb8cd54c31e36743cabd26888cfe34be7356e
Jaime <jaimeloeuf@gmail.com>
2022-11-26 23:59:41 +0800

    ri: Fix file formatting for token_type.

--- ri/src/token_type.rs
+++ ri/src/token_type.rs
@@ -1,69 +1,67 @@
-/**
- * Enum for all possible Token Types.
- *
- * Inherited traits:
- * Debug is to make debug printing easier and skip implementing the display trait
- * PartialEq is used to do comparison instead of match. Refer to Parser struct methods
- * @todo Clone trait is needed as Token struct contains this, and Token struct derives the Clone trait
- */
+/// Enum for all possible Token Types.
+///
+/// Inherited traits:
+/// Debug is to make debug printing easier and skip implementing the display trait
+/// PartialEq is used to do comparison instead of match. Refer to Parser struct methods
+/// @todo Clone trait is needed as Token struct contains this, and Token struct derives the Clone trait
 #[derive(Debug, Clone, PartialEq)]
 pub enum TokenType {
-  // Single-character tokens.
-  Semicolon,    // ;
-  LeftParen,    // (
-  RightParen,   // )
-  LeftBrace,    // {
-  RightBrace,   // }
-  LeftBracket,  // [
-  RightBracket, // ]
-  Comma,
-  Dot,
+    // Single-character tokens.
+    Semicolon,    // ;
+    LeftParen,    // (
+    RightParen,   // )
+    LeftBrace,    // {
+    RightBrace,   // }
+    LeftBracket,  // [
+    RightBracket, // ]
+    Comma,
+    Dot,
 
-  // Math operators
-  Minus,
-  Plus,
-  Slash,
-  Star,
+    // Math operators
+    Minus,
+    Plus,
+    Slash,
+    Star,
 
-  // Operators
-  // One or two character tokens.
-  Bang,
-  BangEqual,
-  Equal,
-  EqualEqual,
-  Greater,
-  GreaterEqual,
-  Less,
-  LessEqual,
+    // Operators
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
 
-  // Logical operators
-  And,
-  Or,
+    // Logical operators
+    And,
+    Or,
 
-  // Literals.
-  Identifier,
-  Str, // String is a reserved keyword in rust
-  Number,
+    // Literals.
+    Identifier,
+    Str, // String is a reserved keyword in rust
+    Number,
 
-  /* Keywords */
-  // Break, // Do we need this? Since no loops? should we even have switch case
-  If,
-  Else,
-  False,
-  Function,
-  Arrow, // =>
+    /* Keywords */
+    // Break, // Do we need this? Since no loops? should we even have switch case
+    If,
+    Else,
+    False,
+    Function,
+    Arrow, // =>
 
-  // For,
-  While,
-  Print,
-  Return,
-  True,
-  Const,
-  Ignore,
+    // For,
+    While,
+    Print,
+    Return,
+    True,
+    Const,
+    Ignore,
 
-  // To remove and introduce Tagged union a.k.a optional T for every T
-  // Or maybe uniqueness type
-  Null,
+    // To remove and introduce Tagged union a.k.a optional T for every T
+    // Or maybe uniqueness type
+    Null,
 
-  Eof,
+    Eof,
 }
