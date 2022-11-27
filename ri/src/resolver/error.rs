73ff5052296532103b7bc493a3d246e88c8b5964
Jaime <jaimeloeuf@gmail.com>
2022-11-27 21:34:23 +0800

    ri: Remove unused resolver error variant.

--- ri/src/resolver/error.rs
+++ ri/src/resolver/error.rs
@@ -2,7 +2,6 @@ use crate::token::Token;
 
 #[derive(Debug)]
 pub enum ResolvingError {
-    InternalError(&'static str),
     UndefinedIdentifier(Token),
     IdentifierAlreadyUsed(Token, String),
     IdentifierAlreadyUsedGlobally(Token, String),
@@ -22,7 +21,6 @@ pub enum ResolvingError {
 impl std::fmt::Display for ResolvingError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            ResolvingError::InternalError(ref message) => write!(f, "{}", message),
             ResolvingError::UndefinedIdentifier(ref token) => write!(
                 f,
                 "[line {}] Cannot access value of identifier '{}' before it is defined",
